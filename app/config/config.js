64d0f946619f81839a9ecf4b2d07272b4cf92cab
aZaaa1406 <rvaldeza2200@alumno.ipn.mx>
2025-03-06 00:03:59 -0600

    variables de entorno db

--- app/config/config.js
+++ app/config/config.js
@@ -4,11 +4,11 @@ dotenv.config();
 export const PORT = process.env.PORT || 4000;
 
 export const conection ={
-    PORT: process.env.PORT || 3306,
-    HOST: process.env.HOST || "localhost",
-    USER: process.env.USER || "root",
-    PASSWORD: process.env.PASSWORD || "root",
-    DB: process.env.DB || "eventspace"
+    PORT: process.env.DB_PORT || 3306,
+    HOST: process.env.DB_HOST || "localhost",
+    USER: process.env.DB_USER || "root",
+    PASSWORD: process.env.DB_PASSWORD || "root",
+    DB: process.env.DB_NAME || "eventspace"
 }
 
 export const SALT = parseInt(process.env.SALT);
