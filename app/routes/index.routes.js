05da41d7bc4891e30395b931a4c75f564098d63f
aZaaa1406 <rvaldeza2200@alumno.ipn.mx>
2025-03-07 19:27:49 -0600

    nuevo commit

--- app/routes/index.routes.js
+++ app/routes/index.routes.js
@@ -3,17 +3,14 @@ import userRoutes from "./user.routes.js";
 export const routes = Router();
 
 routes.use('/users', userRoutes);
-<<<<<<< HEAD
 routes.get('/', (req, res) => {
     res.send('Hello World').status(200).json({
         status:200,
         message:"Hello World"
     });
 });
-=======
 routes.use('/', (req, res)=>{
     res.send("Hello World");
 })
->>>>>>> develop
 
 export default routes;
