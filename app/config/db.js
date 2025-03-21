60b4b8c30793aa0e90011f5f1c426080d8b33482
aZaaa1406 <rvaldeza2200@alumno.ipn.mx>
2025-03-21 11:00:52 -0600

    tiempo de espera ajustado

--- app/config/db.js
+++ app/config/db.js
@@ -9,6 +9,5 @@ export const pool = createPool({
     database: conection.DB,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0,
-    connectTimeout: 10000
+    queueLimit: 0
 })
