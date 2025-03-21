7c758d32b6e8b26d7d8a4843a58c8a930c98b2eb
aZaaa1406 <rvaldeza2200@alumno.ipn.mx>
2025-03-21 04:04:17 -0600

    preuba

--- app/config/config.js
+++ app/config/config.js
@@ -5,7 +5,7 @@ export const PORT = process.env.PORT || 4000;
 
 export const conection ={
     PORT: process.env.DB_PORT || 3306,
-    HOST: process.env.DB_HOST || "localhost",
+    HOST: process.env.DB_HOST || "127.0.0.1",
     USER: process.env.DB_USER || "root",
     PASSWORD: process.env.DB_PASSWORD || "root",
     DB: process.env.DB_NAME || "eventspace"
