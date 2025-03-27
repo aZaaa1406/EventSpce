242f2e4de67d03ed0a320659abd72ed7d7a0eab6
aZaaa1406 <rvaldeza2200@alumno.ipn.mx>
2025-03-26 23:12:28 -0600

    arreglos 2.0

--- app/config/config.js
+++ app/config/config.js
@@ -20,8 +20,10 @@ export const SALT = parseInt(process.env.SALT);
 export const SECRET_KEY_JWT = process.env.SECRET_KEY_JWT;
 
 // Usa el valor de ALLOWEDORIGINS si está definido, de lo contrario usa localhost como valor por defecto
-export const URL = process.env.ALLOWED_ORIGINS.split ? process.env.ALLOWED_ORIGINS.split(',') : 'http://localhost:3000';
-console.log(URL);
+export const URL = process.env.ALLOWED_ORIGINS 
+  ? process.env.ALLOWED_ORIGINS.split(',')
+  : ['http://localhost:3000'];
+
 
 export const EMAIL_USER = process.env.EMAIL_USER;
 export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
